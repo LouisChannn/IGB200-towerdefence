b4a582cd4042eefcfa5bb3e8d673eecbe655bfae
LouisChannn <www.louis1125@yahoo.com>
2026-08-25 04:12:18 +1000

    temp turret sprite and rotation (aiming)
    Added a temporary paintbrush sprite thats no a gif since the gif had some issues. added an aiming system and added the code for it, now the turret will face towards the enemy and when the first targeted enemy exit the attack range it will retarget to the next enemy.

---
+++ Assets/Script/Turret.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEditor;
+
+public class Turret : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform turretRotationPoint;
+    [SerializeField] private LayerMask enemyMask;
+    [Header("Attribute")]
+    [SerializeField] private float targetingRange = 5f;
+    [SerializeField] private float rotationSpeed = 360f;
+    private Transform target;
+
+    private void Update ()
+    {
+        if (target == null)
+        {
+            FindTarget();
+            return;
+        }
+
+        RotateTowardsTarget();
+        if(!CheckIfTargetIsInRange())
+        {
+            target = null;
+        }
+    }
+
+    private void FindTarget ()
+    {
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, targetingRange, (Vector2)transform.position, 0f, enemyMask);
+        if (hits.Length > 0)
+        {
+            target = hits[0].transform;
+        }
+    }
+    
+    private bool CheckIfTargetIsInRange ()
+    {
+        return Vector2.Distance(target.position, transform.position) <= targetingRange;
+    }
+
+    private void RotateTowardsTarget ()
+    {
+        float angle = Mathf.Atan2(target.position.y - transform.position.y, target.position.x - transform.position.x) * Mathf.Rad2Deg - 90f;
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+        turretRotationPoint.rotation = Quaternion.RotateTowards(turretRotationPoint.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Handles.color = Color.cyan;
+        Handles.DrawWireDisc(turretRotationPoint.position, Vector3.up, targetingRange);
+    }
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+   
+}
